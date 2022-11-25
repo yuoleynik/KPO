aa7f4d1d01477403b41848f8a0cb55ca43606f0b
yuoleynik <yuoleynik@edu.hse.ru>
2022-11-26 00:57:10 +0300

    Homework

---
+++ Sem2/Hw1/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name;
+    String presence;
+    int grade;
+
+    public Student(String name) {
+        this.name = name;
+        presence = "Не отмечен(а)";
+    }
+}
